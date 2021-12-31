bec3b45c2c03f099e9f6b2f3142e63be4514ae8a
Aid Thompson <thepandariot@gmail.com>
2021-12-31 11:55:22 +0000

    links to socials added to podc

--- pages/podcast/[id].tsx
+++ pages/podcast/[id].tsx
@@ -9,7 +9,15 @@ import { sampleUserData } from "../../utils/sample-data";
 import Layout from "../../components/Layout";
 import List from "../../components/List";
 import { useMainContext } from "../../context/main";
-import { Typography, useMediaQuery, Grid, Box, Button } from "@mui/material";
+import {
+  Typography,
+  useMediaQuery,
+  Grid,
+  Box,
+  Button,
+  Divider,
+  Card,
+} from "@mui/material";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import { Facebook, Podcasts, YouTube } from "@mui/icons-material";
@@ -18,27 +26,24 @@ type Props = {
 };
 
 const PagePodcast = ({ data }: Props) => {
-  console.log("data is back =>", data.data.body[0]);
-  const [thisEp, setThisEp] = React.useState(null);
+  console.log("data is back =>", data);
   const router = useRouter();
   const { id } = router.query;
-  // const first = data.data.body[0];
-  // const second = data.data.body[1];
-
   const theme = useTheme();
   // const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
 
-  React.useEffect(() => {
-    const first = data.data.body[0].items;
-    const second = data.data.body[1].items;
-    const all = [...first, ...second];
+  // React.useEffect(() => {
+  //   const first = data.data.body[0].items;
+  //   const second = data.data.body[1].items;
+  //   const all = [...first, ...second];
 
-    const res = all.filter((each) => each.episode_slug[0].text === id)[0];
-    setThisEp(res);
-  }, [data]);
+  //   const res = all.filter((each) => each.episode_slug[0].text === id)[0];
+  //   setThisEp(res);
+  // }, [data]);
+
   return (
-    <Layout title="Blog back">
+    <Layout title={`Podcast | ${data.title1[0].text}`}>
       <Typography
         variant="h1"
         color="secondary"
@@ -48,7 +53,7 @@ const PagePodcast = ({ data }: Props) => {
           fontSize: isDesktop ? 60 : 80,
         }}
       >
-        {thisEp && thisEp.title1[0].text}
+        {data && data.title1[0].text}
       </Typography>
       <Grid container spacing={2}>
         <Grid
@@ -59,15 +64,16 @@ const PagePodcast = ({ data }: Props) => {
             pl: isDesktop ? "inherit" : "0px !important",
           }}
         >
-          {thisEp && (
+          {data && (
             <Image
+              onClick={() => window.open(data.video.embed_url)}
               className="blog__mainImage"
-              src={thisEp["podc_ep_twitter_img"].url}
+              src={data["podc_ep_twitter_img"].url}
               alt={
-                thisEp["podc_ep_twitter_img"].alt ||
+                data["podc_ep_twitter_img"].alt ||
                 "blog man talking politics dystopia"
               }
-              {...thisEp["podc_ep_twitter_img"].dimensions}
+              {...data["podc_ep_twitter_img"].dimensions}
             />
           )}
         </Grid>
@@ -79,42 +85,63 @@ const PagePodcast = ({ data }: Props) => {
             pl: isDesktop ? "inherit" : "0px !important",
           }}
         >
-          <Box sx={{ display: "flex", flexDirection: "column", padding: 4 }}>
+          <Card sx={{ display: "flex", flexDirection: "column", padding: 4 }}>
             <Typography
               variant="body2"
-              sx={{ fontSize: isDesktop ? "inherit" : "30px" }}
+              sx={{
+                fontSize: isDesktop ? "inherit" : "30px",
+                fontWeight: isDesktop ? 300 : 400,
+              }}
             >
-              {thisEp && thisEp.description[0].text}
+              {data && data.description[0].text}
             </Typography>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              width: "100%",
-              justifyContent: "space-around",
-            }}
-          >
-            <Image
-              src="/spotify.png"
-              height={40}
-              width={40}
-              alt="spotify logo"
-            />
-            <Image
-              src="/youtube.png"
-              height={40}
-              width={40}
-              alt="youtube logo"
-            />
-            <Image
-              src="/podcast.png"
-              height={40}
-              width={40}
-              alt="podcast app logo"
-            />
-          </Box>
+
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                width: "100%",
+                justifyContent: "space-around",
+                mt: 2,
+              }}
+            >
+              <Image
+                className="podcast__socials"
+                onClick={() =>
+                  window.open(
+                    "https://open.spotify.com/show/6mrjI8ZioqWgcHpNIBa3uU?si=2e878a4de4e14ac3"
+                  )
+                }
+                src="/spotify.png"
+                height={40}
+                width={40}
+                alt="spotify logo"
+              />
+              <Image
+                className="podcast__socials"
+                onClick={() => window.open(data.video.embed_url)}
+                src="/youtube.png"
+                height={40}
+                width={40}
+                alt="youtube logo"
+              />
+              <Image
+                className="podcast__socials"
+                onClick={() =>
+                  window.open(
+                    "https://podcasts.apple.com/gb/podcast/aid-thompsin-other-disappointments/id1543205608"
+                  )
+                }
+                src="/podcast.png"
+                height={40}
+                width={40}
+                alt="podcast app logo"
+              />
+            </Box>
+          </Card>
+
+          <Divider sx={{ mb: 4, width: "90%" }} />
         </Grid>
       </Grid>
     </Layout>
@@ -124,7 +151,16 @@ const PagePodcast = ({ data }: Props) => {
 export async function getServerSideProps(context: any) {
   const endpoint = prismic.getEndpoint("funk27");
   const client = prismic.createClient(endpoint, { fetch });
-  const data = await client.getByUID("page", "podcast");
+  const allPodcastData = await client.getByUID("page", "podcast");
+
+  const firstSeason = allPodcastData.data.body[0].items;
+  const secondSeason = allPodcastData.data.body[1].items;
+  const all = [...firstSeason, ...secondSeason];
+
+  const data = all.filter(
+    (each) => each.episode_slug[0].text === context.params.id
+  )[0];
+
   return { props: { data } };
 }
 

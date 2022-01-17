a2e2009177fc6865c2716ddd40a2f632929ce7ec
Aid Thompson <thepandariot@gmail.com>
2022-01-17 04:41:13 +0000

    heroku podcast pg broken

--- pages/podcast/index.tsx
+++ pages/podcast/index.tsx
@@ -27,8 +27,8 @@ type Props = {
 const PagePodcastIndex = ({ data }: Props) => {
   const theme = useTheme();
   const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
-  const mostRecentEpisode = data?.body[0].items[0];
-  const secondMostRecentEpisode = data?.body[1].items[0];
+  // const mostRecentEpisode = data?.body[0].items[0];
+  // const secondMostRecentEpisode = data?.body[1].items[0];
   const { page } = useAnalytics();
   const { toggleLoading } = useMainContext();
 
@@ -36,7 +36,6 @@ const PagePodcastIndex = ({ data }: Props) => {
     page();
   }, []);
   React.useEffect(() => {
-    // console.log("PAGE PODCAST INDEX CHANGED [data]");
     if (data) {
       toggleLoading(false);
     }
@@ -86,7 +85,7 @@ const PagePodcastIndex = ({ data }: Props) => {
           fontSize: isDesktop ? 200 : 110,
         }}
       >
-        Podcast
+        We'll be right back...
       </Typography>
       <Typography
         variant="h5"
@@ -98,215 +97,6 @@ const PagePodcastIndex = ({ data }: Props) => {
       >
         Aid Thompsin & Other Disappointments
       </Typography>
-      <Grid container spacing={2} sx={{ mb: 2 }}>
-        <Grow in={true}>
-          <Grid item xs={12} md={6}>
-            <ContentCard
-              title={mostRecentEpisode.title1[0].text}
-              description={mostRecentEpisode.description[0].text}
-              backgroundArtworkSrc={mostRecentEpisode.podc_ep_twitter_img.url}
-              artworkAlt={mostRecentEpisode.podc_ep_twitter_img.alt}
-              profileImgSrc={mostRecentEpisode.guest_photo.url}
-              slug={mostRecentEpisode.episode_slug[0].text}
-            />
-          </Grid>
-        </Grow>
-        <Grow in={true}>
-          <Grid item xs={12} md={6} lg={4}>
-            <ContentCard
-              title={secondMostRecentEpisode.title1[0].text}
-              description={secondMostRecentEpisode.description[0].text}
-              backgroundArtworkSrc={
-                secondMostRecentEpisode.podc_ep_twitter_img.url
-              }
-              artworkAlt={secondMostRecentEpisode.podc_ep_twitter_img.alt}
-              profileImgSrc={secondMostRecentEpisode.guest_photo.url}
-              slug={secondMostRecentEpisode.episode_slug[0].text}
-            />
-          </Grid>
-        </Grow>
-      </Grid>
-
-      <MuiDivider prim />
-
-      <Grid container spacing={2}>
-        {data?.body[1].items.slice(2, 10).map((each) => (
-          <Grid item xs={12} md={4} lg={3} key={each.title1[0].text}>
-            <ContentCard
-              firstBatch
-              descriptionLength={128}
-              title={each.title1[0].text}
-              description={each.description[0].text}
-              backgroundArtworkSrc={each.podc_ep_twitter_img.url}
-              artworkAlt={each.podc_ep_twitter_img.alt}
-              profileImgSrc={each.guest_photo.url}
-              slug={each.episode_slug[0].text}
-            />
-          </Grid>
-        ))}
-        <Grid item xs={12} md={6}>
-          <ContentCard
-            title={data?.body[1].items[10].title1[0].text}
-            description={data?.body[1].items[10].description[0].text}
-            backgroundArtworkSrc={
-              data?.body[1].items[10].podc_ep_twitter_img.url
-            }
-            artworkAlt={data?.body[1].items[10].podc_ep_twitter_img.alt}
-            profileImgSrc={data?.body[1].items[10].guest_photo.url}
-            slug={data?.body[1].items[10].episode_slug[0].text}
-          />
-        </Grid>
-        <MuiDivider />
-        <Grid item xs={12} md={4} lg={3}>
-          <Box sx={{ display: "flex", flexDirection: "column" }}>
-            {data?.body[1].items.slice(11, 13).map((each) => (
-              <Card sx={{ mb: 2 }} key={each.title1[0].text}>
-                <Link
-                  href="/podcast/[id]"
-                  as={`/podcast/${each.episode_slug[0].text}`}
-                >
-                  <CardContent>
-                    <Typography
-                      gutterBottom
-                      variant="h5"
-                      component="div"
-                      color="secondary.dark"
-                      sx={{ position: "relative" }}
-                    >
-                      {each.title1[0].text.toLocaleLowerCase()}
-                    </Typography>
-                    <Typography
-                      gutterBottom
-                      variant="body2"
-                      component="div"
-                      color="secondary.light"
-                      sx={{ position: "relative" }}
-                    >
-                      {each.description[0].text}
-                    </Typography>
-                  </CardContent>
-                </Link>
-              </Card>
-            ))}
-          </Box>
-        </Grid>
-
-        {data?.body[1].items.slice(13, 21).map((each) => (
-          <Grid item xs={12} md={4} lg={3} key={each.title1[0].text}>
-            <ContentCard
-              firstBatch
-              descriptionLength={128}
-              title={each.title1[0].text}
-              description={each.description[0].text}
-              backgroundArtworkSrc={each.podc_ep_twitter_img.url}
-              artworkAlt={each.podc_ep_twitter_img.alt}
-              profileImgSrc={each.guest_photo.url}
-              slug={each.episode_slug[0].text}
-            />
-          </Grid>
-        ))}
-        <Grid item xs={12} md={4} lg={3}>
-          <Box sx={{ display: "flex", flexDirection: "column" }}>
-            {data?.body[1].items.slice(21, 23).map((each) => (
-              <Card sx={{ mb: 2 }} key={each.title1[0].text}>
-                <Link
-                  href="/podcast/[id]"
-                  as={`/podcast/${each.episode_slug[0].text}`}
-                >
-                  <CardContent>
-                    <Typography
-                      gutterBottom
-                      variant="h5"
-                      component="div"
-                      color="secondary.dark"
-                      sx={{ position: "relative" }}
-                    >
-                      {each.title1[0].text.toLocaleLowerCase()}
-                    </Typography>
-                    <Typography
-                      gutterBottom
-                      variant="body2"
-                      component="div"
-                      color="secondary.light"
-                      sx={{ position: "relative" }}
-                    >
-                      {each.description[0].text}
-                    </Typography>
-                  </CardContent>
-                </Link>
-              </Card>
-            ))}
-          </Box>
-        </Grid>
-
-        <Grid item xs={12} md={6} lg={4}>
-          <ContentCard
-            title={data?.body[1].items[23].title1[0].text}
-            description={data?.body[1].items[23].description[0].text}
-            backgroundArtworkSrc={
-              data?.body[1].items[23].podc_ep_twitter_img.url
-            }
-            artworkAlt={data?.body[1].items[23].podc_ep_twitter_img.alt}
-            profileImgSrc={data?.body[1].items[23].guest_photo.url}
-            slug={data?.body[1].items[23].episode_slug[0].text}
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <ContentCard
-            title={data?.body[1].items[24].title1[0].text}
-            description={data?.body[1].items[24].description[0].text}
-            backgroundArtworkSrc={
-              data?.body[1].items[24].podc_ep_twitter_img.url
-            }
-            artworkAlt={data?.body[1].items[24].podc_ep_twitter_img.alt}
-            profileImgSrc={data?.body[1].items[24].guest_photo.url}
-            slug={data?.body[1].items[24].episode_slug[0].text}
-          />
-        </Grid>
-      </Grid>
-
-      <MuiDivider prim />
-
-      <Grid
-        container
-        spacing={2}
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        style={{ minHeight: "100vh" }}
-      >
-        {data?.body[2].items.map((each) => (
-          <Grid item xs={12} key={each.title1[0].text}>
-            <Card sx={{ maxWidth: 350 }}>
-              <Link
-                href="/podcast/[id]"
-                as={`/podcast/${each.episode_slug[0].text}`}
-              >
-                <CardContent>
-                  <Typography
-                    gutterBottom
-                    variant="h5"
-                    component="div"
-                    color="secondary.dark"
-                    sx={{ position: "relative" }}
-                  >
-                    {each.title1[0].text.toLocaleLowerCase()}
-                  </Typography>
-                  <Typography
-                    gutterBottom
-                    variant="body2"
-                    component="div"
-                    color="secondary.light"
-                    sx={{ position: "relative" }}
-                  >
-                    {each.description[0].text}
-                  </Typography>
-                </CardContent>
-              </Link>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
     </Layout>
   );
 };
